5322d65870f470c272a3887d38a9358e7b950fa0
brianacharya001 <62633019+brianacharya001@users.noreply.github.com>
2026-01-04 08:25:50 -0500

    Change greeting message to 'Deployment Successful'

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     revision = os.getenv('APP_REVISION', 'unknown')
-    return f"<h1>Hello from Flask!</h1><p>Running Revision: <b>{revision}</b></p>"
+    return f"<h1>Deployment Successful</h1><p>Active Revision: <b>{revision}</b></p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
